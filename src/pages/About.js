852de7918b6dbc4f89bfd4351b6c8178e4d578b2
Lawrence O. Caulker <locaulker@gmail.com>
2023-01-23 13:48:44 -0500

    completed about and error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga nesciunt
+        recusandae corrupti, ipsum non temporibus, natus harum laboriosam
+        sapiente quas libero ducimus! Modi cumque voluptatum, iste ipsa minima
+        aliquid autem perferendis fuga sapiente aspernatur illum, laborum, rerum
+        quaerat totam maiores!
+      </p>
+    </section>
   )
 }
 
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react"
+import { Link } from "react-router-dom"
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
+    <section className="section error-page">
+      <div className="error-container">
+        <h1>Opps! It's a Dead End</h1>
+        <Link to="/" className="btn btn-primary">
+          Back Home
+        </Link>
+      </div>
+    </section>
   )
 }
 
